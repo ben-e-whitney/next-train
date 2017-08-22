c99038559560896f29c9bcc8eef9c9fbf9024644
Ben E. Whitney <ben.e.whitney@post.harvard.edu>
2017-08-22 14:11:19 -0400

    Edit to pass most `mypy` checks.
    The module still does not pass `mypy`'s strict checks. In addition,
    `mypy` complains about the `LINES` and `COLS` attributes of `curses`,
    which it does not know about.

--- tui.py
+++ tui.py
@@ -1,36 +1,36 @@
 import curses
 curses.KEY_ENTER = 10
-
 import itertools
+import typing
 
 class Chooser:
+    CHOICE_INDENTATION: int = 2
+    NUM_QUESTION_LINES: int = 1
+    TAG: str = ' ...'
 
-    CHOICE_INDENTATION = 2
-    NUM_QUESTION_LINES = 1
-    TAG = ' ...'
-
-    def __init__(self, question, choices):
-        self.num_choices = len(choices)
-        self.question = self.truncate(question)
-        pattern = ' ' * self.CHOICE_INDENTATION + '[{{:>{wid}}}] {{}}'.format(
-            wid=len(str(len(choices) - 1))
+    def __init__(self, question: str, choices: typing.Collection[str]) -> None:
+        self.question: str = self.truncate(question)
+        pattern: str = ' ' * self.CHOICE_INDENTATION + (
+            '[{{:>{wid}}}] {{}}'.format(wid=len(str(len(choices) - 1)))
         )
-        self.choices = tuple(map(
+        self.choices: typing.Tuple[str, ...] = tuple(map(
             self.truncate,
             itertools.starmap(pattern.format, enumerate(choices))
         ))
-        self.emphasized = list(itertools.repeat(False, curses.LINES))
+        self.emphasized: typing.List[bool] = list(
+            itertools.repeat(False, curses.LINES)
+        )
         self.emphasized[0] = True
 
     @classmethod
-    def truncate(cls, line):
+    def truncate(cls, line: str) -> str:
         return (
             line if len(line) <= curses.COLS
             else line[: curses.COLS - len(cls.TAG)] + cls.TAG
         )
 
     @classmethod
-    def choice_index(cls, i, j):
+    def choice_index(cls, i: int, j: int) -> int:
         if j < cls.NUM_QUESTION_LINES:
             raise ValueError(
                 'Coordinate {j} does not correspond to a choice.'.format(j=j)
@@ -41,26 +41,26 @@ class Chooser:
                 j - cls.NUM_QUESTION_LINES
             )
 
-    def __call__(self, window):
+    def __call__(self, window) -> int:
         assert len(self.TAG) <= curses.COLS
         assert self.NUM_QUESTION_LINES <= curses.LINES
-        page = 0
-        min_page = 0
-        max_page = (
+        page: int = 0
+        min_page: int = 0
+        max_page: int = (
             len(self.choices) // (curses.LINES - self.NUM_QUESTION_LINES)
         )
-        max_page_max_y = self.NUM_QUESTION_LINES + (
+        max_page_max_y: int = self.NUM_QUESTION_LINES + (
             (len(self.choices) - 1) %
             (curses.LINES - self.NUM_QUESTION_LINES)
         )
-        y = 0
-        min_y = 0
-        max_y = curses.LINES - 1 if page < max_page else max_page_max_y
+        y: int = 0
+        min_y: int = 0
+        max_y: int = curses.LINES - 1 if page < max_page else max_page_max_y
         self.draw_page(window, page)
-        digits = []
-        move_made = False
+        digits: typing.List[int] = []
+        move_made: bool = False
         while True:
-            key = window.getch(0, curses.COLS - 1)
+            key: int = window.getch(0, curses.COLS - 1)
             if key == curses.KEY_DOWN:
                 digits.clear()
                 if y < max_y:
@@ -110,29 +110,29 @@ class Chooser:
                 else:
                     digits.clear()
 
-    def draw_page(self, window, i):
+    def draw_page(self, window, i: int) -> None:
         window.clear()
         self.draw_question(window)
         for j in range(1, curses.LINES):
             self.draw_choice(window, i, j)
 
-    def draw_line(self, window, i, j):
+    def draw_line(self, window, i: int, j: int) -> None:
         if j < self.NUM_QUESTION_LINES:
             self.draw_question(window)
         else:
             self.draw_choice(window, i, j)
 
-    def attribute(self, j):
+    def attribute(self, j: int) -> int:
         return curses.A_STANDOUT if self.emphasized[j] else curses.A_NORMAL
 
-    def draw_question(self, window):
+    def draw_question(self, window) -> None:
         window.addstr(0, 0, self.question, self.attribute(0))
 
-    def draw_choice(self, window, i, j):
-        k = self.choice_index(i, j)
+    def draw_choice(self, window, i: int, j: int) -> None:
+        k: int = self.choice_index(i, j)
         if k < len(self.choices):
             window.addstr(j, 0, self.choices[k], self.attribute(j))
 
-    def toggle(self, window, i, j):
+    def toggle(self, window, i: int, j: int) -> None:
         self.emphasized[j] = not self.emphasized[j]
         self.draw_line(window, i, j)
